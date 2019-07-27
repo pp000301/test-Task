a2848758aa94ed00e3ff25213ab85381250e0332
Valerii <pp000302@gmail.com>
2019-07-27 21:45:25 +0300

    Fixed incorrect return values

--- src/main/java/LockersValidator.java
+++ src/main/java/LockersValidator.java
@@ -9,15 +9,13 @@ public class LockersValidator {
 
     private static char lockCharacter = '{';
     private static char unlockCharacter = '}';
-    private static String inputValidationRegex = "\\{.+?\\}+";
-    private static String maxLockerExtractRegex = "\\{.+\\}";
+    private static String inputValidationRegex = "\\{.*\\}+";
 
     /**
-     * @description Validation of input String
-     * @author  Valery Vorobei
-     *
      * @param input The source String expression
-     * @return  Return validate  Set<String>
+     * @return Return validate  Set<String>
+     * @description Validation of input String
+     * @author Valery Vorobei
      */
     public static Set<String> validate(String input) {
 
@@ -25,7 +23,6 @@ public class LockersValidator {
         int numberOfRedundantLockers, excessLockerCount;
         ValidationParams params = new ValidationParams();
         params.input = input;
-
         Set<String> resultSet = new HashSet<>();
 
         // list of indexes for redundant lockers in input text
@@ -38,20 +35,21 @@ public class LockersValidator {
         Pattern pattern = Pattern.compile(inputValidationRegex);
         Matcher matcher = pattern.matcher(input);
 
-        if (matcher.find() == false) {
-            resultSet.add("");
+        if (getCountChar(input, lockCharacter) == 0 & getCountChar(input, unlockCharacter) == 0) {
+            resultSet.add(input);
+            return resultSet;
+        } else if ((getCountChar(input, lockCharacter) == 0 & getCountChar(input, unlockCharacter) != 0) | (getCountChar(input, lockCharacter) != 0 & getCountChar(input, unlockCharacter) == 0)) {
+            String res = extractString(input, "[^\\}\\{]*");
+            resultSet.add(res);
+            return resultSet;
+        } else if (matcher.find() == false) {
+            resultSet.add(extractString(input, "[^\\}\\{]*"));
             return resultSet;
         } else {
 
             // extract the longest valid locker value
 
-            pattern = Pattern.compile(maxLockerExtractRegex);
-            matcher = pattern.matcher(input);
-
-            while (matcher.find()) {
-                input = matcher.group();
-            }
-
+            input = extractString(input, inputValidationRegex);
 
             lockCount = getCountChar(input, lockCharacter);
             unlockCount = getCountChar(input, unlockCharacter);
@@ -60,6 +58,15 @@ public class LockersValidator {
             if (lockCount > unlockCount) {
                 redundantLockersIndexes = indexsChar(input, lockCharacter);
                 numberOfRedundantLockers = Math.toIntExact(lockCount);
+            } else if (lockCount == unlockCount) {
+                String res = params.input;
+                String prefix = extractString(res, "^[^\\{]*");
+                prefix = extractString(prefix, "\\w*");
+                String syf = extractString(res, "[^\\}]*$");
+                syf = extractString(syf, "\\w*");
+                res = prefix + extractString(res, inputValidationRegex) + syf;
+                resultSet.add(res);
+                return resultSet;
             } else {
                 redundantLockersIndexes = indexsChar(input, unlockCharacter);
                 numberOfRedundantLockers = Math.toIntExact(unlockCount);
@@ -76,18 +83,33 @@ public class LockersValidator {
 
     }
 
+    public static String extractString(String original, String s) {
+        Pattern pattern;
+        Matcher matcher;
+        String inputPrefix = "";
+        pattern = Pattern.compile(s);
+        matcher = pattern.matcher(original);
+
+        while (matcher.find()) {
+            String inputPrefixTemp = matcher.group();
+            if (!inputPrefixTemp.isEmpty()) {
+                inputPrefix = inputPrefix + inputPrefixTemp;
+            }
+        }
+        return inputPrefix;
+    }
+
     /**
+     * @param params ValidationParams instance to include all necessary validation parameters
+     * @return Set<String> Result set of strings that contain
      * @description Method for final validation of input String in case if there are extra locker/unlocker characters are
      * defined in the end of input
      * @author Valeriy Vorobey
-     *
-     * @param params ValidationParams instance to include all necessary validation parameters
-     * @return Set<String> Result set of strings that contain
      */
     private static Set<String> validateString(ValidationParams params) {
 
         Set<String> result = new HashSet<>();
-        String prefix="";
+        String prefix = "";
 
         List<Integer> arr = null;
 
@@ -97,25 +119,28 @@ public class LockersValidator {
 
         for (int i = 0; i < params.redundantLockersCombinations.size(); i = i + params.excessLockerCount) {
 
-            String outer = params.input;
-            Pattern pattern = Pattern.compile("^[^\\{]*");
-            Matcher matcher = pattern.matcher(outer);
+            prefix = extractString(params.input, "^[^\\{]*");
+            int longPrefix = prefix.length();
 
-            while (matcher.find()) {
-              prefix = matcher.group();
-            }
-
-            StringBuffer stringBuffer = new StringBuffer(outer);
+            StringBuffer stringBuffer = new StringBuffer(params.input);
 
             int ofsetWhenDeleting = 0;
 
             for (int j = i; j < i + params.excessLockerCount; j++) {
                 int m = params.redundantLockersIndexes.get(params.redundantLockersCombinations.get(j) - 1) - ofsetWhenDeleting;
-                stringBuffer.deleteCharAt(m+prefix.length());
+                stringBuffer.deleteCharAt(m + longPrefix);
                 ofsetWhenDeleting++;
             }
 
-            if (isLockUnlockCharsEquals(stringBuffer.toString())) result.add(stringBuffer.toString());
+            if (isLockUnlockCharsEquals(stringBuffer.toString())) {
+                String res = stringBuffer.toString();
+                prefix = extractString(res, "^[^\\{]*");
+                prefix = extractString(prefix, "\\w*");
+                String syf = extractString(res, "[^\\\\}^]*$");
+                syf = extractString(syf, "\\w*");
+                res = prefix + extractString(res, inputValidationRegex) + syf;
+                result.add(res);
+            }
         }
 
         return result;
@@ -125,7 +150,6 @@ public class LockersValidator {
     private static long getCountChar(String input, char c) {
         return input.chars().filter(ch -> ch == c).count();
     }
-
 
     // get indexs char in string
     private static List<Integer> indexsChar(String str, Character character) {
@@ -143,11 +167,11 @@ public class LockersValidator {
     }
 
     /**
-     * @description Generate combinations from m at n remove the extra characters
      * @param arr Intermediate collection
      * @param m
      * @param n
      * @return List combinations
+     * @description Generate combinations from m at n remove the extra characters
      */
     private static List<Integer> generateCombinations(List<Integer> arr, int m, int n) {
         if (arr == null) {
@@ -167,19 +191,19 @@ public class LockersValidator {
     }
 
     // isLockUnlockCharsEquals
-    private static boolean isLockUnlockCharsEquals(String input){
+    private static boolean isLockUnlockCharsEquals(String input) {
 
         boolean isInputValid = false;
 
-        Pattern pattern = Pattern.compile(maxLockerExtractRegex);
-        Matcher  matcher = pattern.matcher(input);
+        Pattern pattern = Pattern.compile(inputValidationRegex);
+        Matcher matcher = pattern.matcher(input);
 
         while (matcher.find()) {
             input = matcher.group();
         }
 
-        long numberOfLockers = getCountChar(input,lockCharacter);
-        long numberOfUnlockers = getCountChar(input,unlockCharacter);
+        long numberOfLockers = getCountChar(input, lockCharacter);
+        long numberOfUnlockers = getCountChar(input, unlockCharacter);
 
         if (numberOfUnlockers == numberOfLockers) {
             isInputValid = true;
@@ -189,8 +213,7 @@ public class LockersValidator {
 
 
     // inner class with validation params
-    private static class ValidationParams
-    {
+    private static class ValidationParams {
         public String input;
         public List<Integer> redundantLockersIndexes;
         public ArrayList<Integer> redundantLockersCombinations;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         Set<String> resultSet;
-        String text = "asas{ccc{cc{cc}}sdff";
+        String text = "}}}a{{{}}{{}}{}mdnfn{{{";
         resultSet = LockersValidator.validate(text);
         System.out.println(resultSet.size());
         for (String result : resultSet) {
             System.out.println(result);
         }
-
-
     }
 }
